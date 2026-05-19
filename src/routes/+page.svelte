5417895c7d96b958d4cf20c204389735aedfe4d2
Yvan van der Wal <y.a.van.der.wal@gasunie.nl>
2026-05-19 11:02:22 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import axios from 'axios';
+	const ufcUrl = 'http://ufcstats.com/statistics/fighters';
+
+	async function fetchData() {
+		const res = await axios.get(ufcUrl, );
+		console.log(res.data);
+	}
+</script>
+
+<button class="rounded-2xl bg-blue-400 p-2 px-3" onclick={fetchData}>fetch data</button>
